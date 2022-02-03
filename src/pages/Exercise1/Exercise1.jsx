bc07002e8504bea6cc4c7d5b5a366da5e32d0df6
Ing. Nelson <nescalan@gmail.com>
2022-02-03 14:56:43 -0600

    Note: Completed exercise 1

--- src/pages/Exercise1/Exercise1.jsx
+++ src/pages/Exercise1/Exercise1.jsx
@@ -3,7 +3,9 @@ const Exercise1 = () => {
   return (
     <section className="exercise1">
       <h1>Exercise 1: Hello World!</h1>
-      <div className="yellow-square"></div>
+      <div className="yellow-square">
+        <p>Hello world</p>
+      </div>
     </section>
   );
 };
