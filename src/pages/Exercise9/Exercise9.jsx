3c24191ee9ae5007db14e48b072d8c636388f382
Nelson <nescalan@gmail.com>
2022-02-07 16:41:43 -0600

    Deploy: feb 7, 16: 45

--- src/pages/Exercise9/Exercise9.jsx
+++ src/pages/Exercise9/Exercise9.jsx
@@ -2,6 +2,13 @@ const Exercise9 = () => {
   return (
     <div className="container">
       <h1>Exercise 9: Working with an API</h1>
+      <div>
+        <p>
+          Every React or any frontend developer must learn how to work with an
+          API. When building applications, you receive data from the backend or
+          an outside source which you will have to present nicely in the UI.
+        </p>
+      </div>
     </div>
   );
 };
